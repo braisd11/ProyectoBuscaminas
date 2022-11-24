bba2c5604abe6e1ca439b86371d69bfa305839fa
NCrysys <118912702+NCrysys@users.noreply.github.com>
2022-11-24 13:03:45 +0100

    Add files via upload
    XeradorMenus0.2

--- CaraLibro/src/main/java/com/mycompany/caralibro/XeradorMenus.java
+++ CaraLibro/src/main/java/com/mycompany/caralibro/XeradorMenus.java
@@ -16,14 +16,24 @@ public class XeradorMenus {
         Scanner entrada = new Scanner(System.in);
         System.out.println("1-Crear un novo perfil");
         System.out.println("2-Iniciar sesión");
+        System.out.print("Seleccione unha opción->");
         int n=entrada.nextInt();
-        while (n!=1 && n!=2) {
+        while (n<1 || n>2) {
             System.out.println("Opción no válida, seleccione de novo:");
             n=entrada.nextInt();
         }
     }
     
-    public void mostrarMenuPrincipal() {
+    public void mostrarMenuPrincipal(Perfil p) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("1-Crear un novo perfil");
+        System.out.println("2-Iniciar sesión");
+        System.out.print("Seleccione unha opción->");
+        int n=entrada.nextInt();
+        while (n<1 || n>6) {
+            System.out.println("Opción no válida, seleccione de novo:");
+            n=entrada.nextInt();
+        }
         //Estado
         //Biografía
         //Solicitudes de Amizade
@@ -31,19 +41,33 @@ public class XeradorMenus {
         //Mensaxes
         //Pechar a sesión
     }
+    
+    public void mostrarBiografía(Perfil p) {
+        for (int i = 0; i < p.publicacions.size(); i++) {
+            System.out.println(p.publicacions.get(i).getTexto);
+        }
+    }
+    
     public void mostrarSolicitudesAmizade(Perfil p) {
         for (int i = 0; i < p.solicitudes.size(); i++) {
             System.out.println(p.solicitudes.get(i).getNome);
         }
     }
+    
     public void mostrarListaAmigos(Perfil p) {
         for (int i = 0; i < p.amigos.size(); i++) {
             System.out.println(p.amigos.get(i).getNome);
         }
     }
+    
     public void mostrarMensaxes(Perfil p) {
         for (int i = 0; i < p.mensaxes.size(); i++) {
             System.out.println(p.mensaxes.get(i).getTexto + "de" + p.mensaxes.get(i).getRemitente);
         }
     }
+    
+    public void pecharSesión() {
+        mostrarMenuInicial();
+    }
+    
 }
